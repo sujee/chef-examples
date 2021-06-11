82bf61f48826b76d911858673f963c726541f22c
Sujee Maniyam <sujee@elephantscale.com>
2021-06-11 20:43:11 +0000

    updated kitchen for ip addresses

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -1,8 +1,25 @@
-package "apache2" do 
-  action :install
+#package "apache2" do 
+#  action :install
+#end
+
+package 'apache2' do
+  case node[:platform]
+  when 'redhat','centos'
+     package_name 'httpd'
+     action    :install
+  when 'ubuntu','debian'
+     package_name 'apache2'
+     action :install
+  end
 end
 
 service "apache2"  do
+  case node[:platform]
+  when 'redhat','centos'
+     service_name 'httpd'
+  when 'ubuntu','debian'
+     service_name 'apache2'
+  end
   action [ :enable,  :start ]
 end
 
